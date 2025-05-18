61107bc78a32136680ca8276f5de5ac48dce5e27
LainerDonet <lainerdonet87@gmail.com>
2025-05-17 19:07:51 -0600

    javascript M15

---
+++ Javascript M15/script.js
@@ -0,0 +1,8 @@
+function add(x,y) {
+    console.log(`The sum of ${x} and ${y} is ${x+y}`);
+    return x + y;
+}
+add(5, 10);
+const result = add(5 + 3, 10);
+console.log(`The result is ${result}`);
+
